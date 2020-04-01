3f88e3a0d71cda250c09dbdf9db80b8a47d3f492
Toby Horn <kankuro865@gmail.com>
2020-03-31 22:42:32 -0500

    Added comments to Reactions Controller

--- controllers/ReactionsController.js
+++ controllers/ReactionsController.js
@@ -1,4 +1,4 @@
-const {verify_msg_id, server_id} = require('../config');
+const {server_id} = require('../config');
 
 // Create a new module export
 module.exports = {
@@ -9,14 +9,18 @@ module.exports = {
         const message = m, args = a;
         const verifyChannel = message.guild.channels.cache.find((c => c.name.includes("verify"))); //verify channel
 
+        // Fetch the message from the verify channel
         verifyChannel.messages.fetch({limit:1}).then((messages) => {
+            // If there is already a message in the channel don't create a new one
             if(messages.size !== 0) {
-                message.channel.send(`There is already a verify message!`)
+                return message.channel.send(`There is already a verify message!`);
             } else {
+                // Make sure the arg is "create" and author is server owner
                 if(args.length === 1 && args[0] === "create" && message.guild.ownerID === message.author.id) {
+                    // Send the message
                     verifyChannel.send("Please React with <a:cube:694295496885534730> to verify that you are a human!")
                     .then(sent => {
-                        verifyMessageId = sent.id;
+                        // React to the message with the proper emoji
                         sent.react("694295496885534730");
                     });
                 }
@@ -26,27 +30,33 @@ module.exports = {
     },
     verifyHandler: function(r, u) {
         // Create vars
-        const user = u;
-        const reaction = r;
+        const user = u, reaction = r;
         const client = reaction.client;
-        const guild = client.guilds.cache.find((g => g.id === server_id));
-        const verifyChannel = guild.channels.cache.find((c => c.name.includes("verify")));
-        const role = guild.roles.cache.find(r => r.name === "Users");
+        const guild = client.guilds.cache.find((g => g.id === server_id)); //guild
+        const verifyChannel = guild.channels.cache.find((c => c.name.includes("verify"))); //verify channel
+        const role = guild.roles.cache.find(r => r.name === "Users"); //Users role
 
+        // Fetch the message from the verify channel
         verifyChannel.messages.fetch({limit:1}).then(message => {
 
+            // If the user is a bot or reacted to the wrong messag, ignore
             if(user.bot) {
                 return;
             } else if(reaction.message.id !== message.first().id) {
                 return;
             };
     
+            // Get fetch the member with the proper user id
             guild.members.fetch(user.id).then((member) => {
                 
+                // Ensure the reaction is the right emoji
                 if(reaction.emoji.id === "694295496885534730") {
+                    // Add the member to the role
                     member.roles.add(role);
+                    // Remove the reaction
                     reaction.remove();
                 } else {
+                    // Remove the reaction 
                     reaction.remove();
                 } 
             });
