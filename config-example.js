6d1a944860aa2e1f7594d0418d56ac49c37b024b
Toby Horn <kankuro865@gmail.com>
2020-04-08 20:10:10 -0500

    Updated config-example

--- config-example.js
+++ config-example.js
@@ -1,10 +1,10 @@
 module.exports = {
-    "token" : process.env.BOT_TOKEN,
-    "prefix" : process.env.PREFIX_CHARACTER || "!",
+    "token": process.env.BOT_TOKEN,
+    "prefix": process.env.PREFIX_CHARACTER || "!",
 
-    "server_id" : process.env.SERVER_ID,
-    "verify_emoji_name" : process.env.EMOJI_NAME,
-    "verify_emoji_id" : process.env.EMOJI_ID,
+    "server_id": process.env.SERVER_ID,
+    "verify_emoji_name": process.env.EMOJI_NAME,
+    "verify_emoji_id": process.env.EMOJI_ID,
 
     "admin_role": process.env.ADMINISTRATOR_ROLE_NAME,
     "super_role": process.env.SUPER_MODERATOR_ROLE_NAME,
@@ -18,12 +18,12 @@ module.exports = {
     "action_log_channel": process.env.MODERATOR_LOG_CHANNEL_NAME,
     "join_log_channel" : process.env.JOIN_LOG_CHANNEL_NAME,
 
-    "excluded_trigger_channels" : [],
-    "special_permission_flags" : [],
+    "excluded_trigger_channels": [],
+    "special_permission_flags": [],
 
-    "db_name" : process.env.MYSQL_DATABASE || "beaconbot",
-    "db_host" : process.env.DATABASE_HOSTNAME || "localhost",
-    "db_port" : process.env.DATABASE_PORT || 3306,
-    "db_user" : process.env.MYSQL_USER || "user",
-    "db_pass" : process.env.MYSQL_PASSWORD || "secret"
+    "db_name": process.env.MYSQL_DATABASE || "beaconbot",
+    "db_host": process.env.DATABASE_HOSTNAME || "localhost",
+    "db_port": process.env.DATABASE_PORT || 3306,
+    "db_user": process.env.MYSQL_USER || "user",
+    "db_pass": process.env.MYSQL_PASSWORD || "secret"
 }
