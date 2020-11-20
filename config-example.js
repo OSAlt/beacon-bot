bba5a218bdddbb35f339f17d3cafc0098f20f89a
Toby Horn <kankuro865@gmail.com>
2020-11-19 23:48:38 -0600

    Reworked the bot settings to use the db instead of the config file Edited the config-example.js & env.template files to reflect on above changes Reworked all command and controller files to use the settings from the db instead of config files Removed verified members from server info removed unneeded code from usage.js Fixed recent warnings to now include warnings from kicks and bans Added the ability to disable/enable commands (also added fields for further code to allow other aspects of the commands to be changed) Changed role to be mod only Added ability to add new auto and joinable roles by id instead of role tag or name Removed code to exclude pokecord channel from edit log Fixed bug in nickname command Fixed bug that would cause the temp voice channels to not delete if the last user changes channels instead of disconnecting

--- config-example.js
+++ config-example.js
@@ -1,29 +1,9 @@
 module.exports = {
     "token" : process.env.BOT_TOKEN,
-    "prefix" : process.env.PREFIX_CHARACTER || "!",
-
-    "server_id" : process.env.SERVER_ID,
-
-    "admin_role": process.env.ADMINISTRATOR_ROLE,
-    "super_role": process.env.SUPER_MODERATOR_ROLE,
-    "mod_role": process.env.MODERATOR_ROLE,
-    "mod_trainee_role": process.env.MOD_TRAINEE_ROLE,
-    "user_role": process.env.USER_ROLE,
-
-    "admin_channel": process.env.ADMINISTRATOR_CHANNEL_NAME,
-    "super_channel": process.env.SUPER_MODERATOR_CHANNEL_NAME,
-    "mod_channel": process.env.MODERATOR_CHANNEL_NAME,
-    "super_log_channel": process.env.SUPER_MODERATOR_LOG_CHANNEL_NAME,
-    "action_log_channel": process.env.MODERATOR_LOG_CHANNEL_NAME,
-    "join_log_channel" : process.env.JOIN_LOG_CHANNEL_NAME,
-
-    "excluded_trigger_channels" : process.env.EXCLUDED_CHANNELS.split(","),
-    "url_role_whitelist" : process.env.URL_ROLE_WHITELIST.split(","),
-    "special_permission_flags" : process.env.SPECIAL_PERMISSION_FLAGS.split(","),
-    
     "db_name" : process.env.MYSQL_DATABASE || "beaconbot",
     "db_host" : process.env.DATABASE_HOSTNAME || "localhost",
     "db_port" : process.env.DATABASE_PORT || 3306,
     "db_user" : process.env.MYSQL_USER || "user",
-    "db_pass" : process.env.MYSQL_PASSWORD || "secret"
+    "db_pass" : process.env.MYSQL_PASSWORD || "secret",
+    "server_id" : process.env.SERVER_ID
 }
